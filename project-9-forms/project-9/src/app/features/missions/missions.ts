80db57aea9059fe7a983b1e6c3e23edd20cec511
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2026-04-18 14:17:23 +0300

    create a directive and use it

--- project-9-forms/project-9/src/app/features/missions/missions.ts
+++ project-9-forms/project-9/src/app/features/missions/missions.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MissionsService } from '../../services/missions.service.js';
 import { Mission } from '../../interfaces/mission.interface.js';
 import { RouterLink } from "@angular/router";
+import { HighlightDirective } from '../../directives/highlight.directive.js';
 
 @Component({
   selector: 'app-missions',
-  imports: [RouterLink],
+  imports: [RouterLink, HighlightDirective],
   templateUrl: './missions.html',
   styleUrl: './missions.css',
 })
